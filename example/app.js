aa1faab46cc25e56ef3f2afb995b04efa56e9808
thedammyking <kingdammie@gmail.com>
2019-12-08 18:04:16 +0100

    prod build

--- example/app.js
+++ example/app.js
@@ -1,24 +1,24 @@
 var http = require('http');
-var requestListener = function(req, res) {
+var requestListener = function (req, res) {
 	res.writeHead(200);
 	res.end('Hello, World!');
 };
 
 var Feed = require('../index');
 
-Feed.convert('https://medium.com/feed/agent-banking', function(err, json) {
+Feed.convert('https://medium.com/feed/agent-banking', function (err, json) {
 	return console.log('Synchronous', json);
 });
 
 Feed.convert('https://medium.com/feed/agent-banking')
-	.then(function(json) {
+	.then(function (json) {
 		return console.log('Asynchronous', json);
 	})
-	.catch(function(err) {
+	.catch(function (err) {
 		console.log(err);
 	});
 
 var server = http.createServer(requestListener);
-server.listen(3000, function() {
+server.listen(3000, function () {
 	console.log('Listening on port 3000');
 });
